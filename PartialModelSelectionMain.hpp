4c8fbb20ba090627a1e6af29685adeb6071cc04e
Joe Vargovich <jrv233@nau.edu>
2020-06-11 12:44:10 -0700

    Updated include again.

--- PartialModelSelectionMain.hpp
+++ PartialModelSelectionMain.hpp
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 #include "PartialModelSelection.hpp"
-=======
-#include "PartialModelSelection.hpp";
->>>>>>> 35f7e9d07ec6937f4f39e2761a7a2ad3db092295
 void testMinimize(ModelSelectionMap testMap, double lowModelSize, double highModelSize, double penaltyQuery);
 void testGetPen(ModelSelectionMap testMap, double expectedPenalty);
